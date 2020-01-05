42e667b5dd35c42c026cee93d2b7484df4de04fd
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:03:49 -0500

    feat: store: b__ factories

--- store.d.ts
+++ store.d.ts
@@ -1,8 +1,19 @@
-export declare const __a1__pathname__medium__source: import("svelte/store").Writable<any>;
-export declare const __arr__pathname__medium__source: import("svelte/store").Writable<any>;
-export declare function reload__a1__pathname__medium__source(): Promise<void>;
-export declare const __idx__pathname__medium: import("svelte/store").Writable<number>;
-export declare const __a1__pathname__medium: import("svelte/store").Readable<any>;
+import { Writable } from 'svelte/store';
+import { falsy } from '@ctx-core/function';
+export interface Writable__a1__pathname__medium__source extends Writable<falsy | string[]> {
+    reload__a1__pathname__medium__source: () => void;
+}
+export declare const b__a1__pathname__medium__source: (ctx?: any, opts?: any) => any;
+export declare const __a1__pathname__medium__source: any;
+export declare const reload__a1__pathname__medium__source: any;
+export interface Writable__idx__pathname__medium extends Writable<number> {
+    next__pathname__medium: () => void;
+    prev__pathname__medium: () => void;
+}
+export declare const b__idx__pathname__medium: (ctx?: any, opts?: any) => Writable__idx__pathname__medium;
+export declare const __idx__pathname__medium: Writable__idx__pathname__medium;
+export declare const next__pathname__medium: () => void, prev__pathname__medium: () => void;
+export declare const b__a1__pathname__medium: (ctx?: any, opts?: any) => import("svelte/store").Readable<unknown>;
+export declare const __a1__pathname__medium: import("svelte/store").Readable<unknown>;
+export declare const b__pathname__medium: (ctx?: any, opts?: any) => import("svelte/store").Readable<any>;
 export declare const __pathname__medium: import("svelte/store").Readable<any>;
-export declare function next__pathname__medium(): void;
-export declare function prev__pathname__medium(): void;
--- store.ts
+++ store.ts
@@ -1,36 +1,70 @@
-import { writable, derived, get } from 'svelte/store'
+import { writable, derived, get, Writable } from 'svelte/store'
 import { _has__dom } from '@ctx-core/dom'
 import { _idx__prev, _idx__next } from '@ctx-core/array'
 import { _a1__pathname__medium } from './fetch'
-export const __a1__pathname__medium__source = writable(null)
-export const __arr__pathname__medium__source = __a1__pathname__medium__source
-if (_has__dom()) {
-	reload__a1__pathname__medium__source()
+import { _b, assign } from '@ctx-core/object'
+import { falsy } from '@ctx-core/function'
+export interface Writable__a1__pathname__medium__source extends Writable<falsy|string[]> {
+	reload__a1__pathname__medium__source:()=>void
 }
-export async function reload__a1__pathname__medium__source() {
-	const a1__pathname__medium = await _a1__pathname__medium()
-	__a1__pathname__medium__source.set(a1__pathname__medium)
+export const b__a1__pathname__medium__source = _b('__a1__pathname__medium__source', ()=>{
+	const __a1__pathname__medium__source = writable(null)
+	if (_has__dom()) {
+		reload__a1__pathname__medium__source()
+	}
+	return assign(__a1__pathname__medium__source, {
+		reload__a1__pathname__medium__source,
+	})
+	async function reload__a1__pathname__medium__source() {
+		const a1__pathname__medium = await _a1__pathname__medium()
+		__a1__pathname__medium__source.set(a1__pathname__medium)
+	}
+})
+export const __a1__pathname__medium__source = b__a1__pathname__medium__source()
+export const {
+	reload__a1__pathname__medium__source,
+} = __a1__pathname__medium__source
+export interface Writable__idx__pathname__medium extends Writable<number> {
+	next__pathname__medium:()=>void
+	prev__pathname__medium:()=>void
 }
-export const __idx__pathname__medium = writable(0)
-export const __a1__pathname__medium =
-	derived(__a1__pathname__medium__source,
-		a1__pathname__medium__source => a1__pathname__medium__source || [])
-export const __pathname__medium = derived([
-		__a1__pathname__medium,
-		__idx__pathname__medium
-	],
-	([a1__pathname__medium, idx__pathname__medium]) =>
-		a1__pathname__medium[idx__pathname__medium]
-)
-export function next__pathname__medium() {
-	const { length } = get(__a1__pathname__medium)
-	__idx__pathname__medium.update(
-		idx__pathname__medium => _idx__next(length, idx__pathname__medium)
-	)
-}
-export function prev__pathname__medium() {
-	const { length } = get(__a1__pathname__medium)
-	__idx__pathname__medium.update(
-		idx__pathname__medium => _idx__prev(length, idx__pathname__medium)
-	)
-}
+export const b__idx__pathname__medium = _b<Writable__idx__pathname__medium>('__idx__pathname__medium', ()=>{
+	const __idx__pathname__medium = writable(0)
+	return assign(__idx__pathname__medium, {
+		next__pathname__medium,
+		prev__pathname__medium,
+	})
+	function next__pathname__medium() {
+		const { length } = get(__a1__pathname__medium)
+		__idx__pathname__medium.update(
+			idx__pathname__medium=>_idx__next(length, idx__pathname__medium)
+		)
+	}
+	function prev__pathname__medium() {
+		const { length } = get(__a1__pathname__medium)
+		__idx__pathname__medium.update(
+			idx__pathname__medium=>_idx__prev(length, idx__pathname__medium)
+		)
+	}
+	return __idx__pathname__medium
+})
+export const __idx__pathname__medium = b__idx__pathname__medium()
+export const {
+	next__pathname__medium,
+	prev__pathname__medium,
+} = __idx__pathname__medium
+export const b__a1__pathname__medium = _b('__a1__pathname__medium', ctx=>
+	derived(b__a1__pathname__medium__source(ctx),
+		a1__pathname__medium__source=>
+			a1__pathname__medium__source
+			|| []))
+export const __a1__pathname__medium = b__a1__pathname__medium()
+export const b__pathname__medium = _b('__pathname__medium', ctx=>
+	derived([
+			b__a1__pathname__medium(ctx),
+			b__idx__pathname__medium(ctx),
+		],
+		([a1__pathname__medium, idx__pathname__medium])=>
+			a1__pathname__medium[idx__pathname__medium]
+	))
+export const __pathname__medium = b__pathname__medium()
