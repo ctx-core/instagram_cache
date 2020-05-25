d9c21fb52d26d3651f4fb11f7fb6ad51e450e2fa
Brian Takita <brian.takita@gmail.com>
2020-05-24 20:44:12 -0700

    - esm

--- bin/cache-instagram.js
+++ bin/cache-instagram.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-require = require('esm')(module)
-const command_line_args = require('command-line-args')
+import command_line_args from 'command-line-args'
 const options = command_line_args([
 	{ name: 'reload', alias: 'r', type: Boolean, description: 'Reload full cache.' },
 	{ name: 'help', alias: 'h', description: 'Print this usage guid.' },
 ])
-const { put__cache__scrape__webdriver } = require('@ctx-core/instagram_cache')
+import { put__cache__scrape__webdriver } from '@ctx-core/instagram_cache'
 put__cache__scrape__webdriver(options)
