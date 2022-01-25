31a2dab3989e5b54dce517c93d539ef240b55075
Brian Takita <brian.takita@gmail.com>
2022-01-24 20:57:37 -0600

    remove () around single argument arrow functions

--- src/medium_pathname$_b.js
+++ src/medium_pathname$_b.js
@@ -4,7 +4,7 @@ import { medium_pathname_a$_b } from './medium_pathname_a$_b.js'
 import { medium_pathname_idx$_b } from './medium_pathname_idx$_b.js'
 const key = 'medium_pathname$'
 /** @type {import(medium_pathname$_b).medium_pathname$_b} */
-export const medium_pathname$_b = be_(key, (ctx)=>{
+export const medium_pathname$_b = be_(key, ctx=>{
 	return computed$([
 		medium_pathname_a$_b(ctx),
 		medium_pathname_idx$_b(ctx)
