5643de6ca3be81a56eb9981687a24f044df5d020
Brian Takita <brian.takita@gmail.com>
2022-06-28 10:05:42 -0500

    import { type ... }

--- src/medium_pathname__.d.ts
+++ src/medium_pathname__.d.ts
@@ -1,4 +1,4 @@
-import { ReadableAtom_ } from '@ctx-core/nanostores'
-import { B } from '@ctx-core/object'
+import { type ReadableAtom_ } from '@ctx-core/nanostores'
+import { type B } from '@ctx-core/object'
 export declare const medium_pathname__:B<ReadableAtom_<string>>
 export { medium_pathname__ as medium_pathname$_ }
--- src/medium_pathname_idx__.d.ts
+++ src/medium_pathname_idx__.d.ts
@@ -1,5 +1,5 @@
-import { WritableAtom_ } from '@ctx-core/nanostores'
-import { B } from '@ctx-core/object'
+import { type WritableAtom_ } from '@ctx-core/nanostores'
+import { type B } from '@ctx-core/object'
 export declare const medium_pathname_idx__:B<medium_pathname_idx__T>
 export interface medium_pathname_idx__T extends WritableAtom_<number> {
 	next_medium_pathname:()=>void;
